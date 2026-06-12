068f18dadca777196ca6c48ec91d53796a071d98
phanqig <phuocnghia02052006@gmail.com>
2026-06-12 23:44:09 +0700

    add tables

--- backend/app/models.py
+++ backend/app/models.py
@@ -11,5 +11,25 @@ class WebUser(SQLModel, table = True):
 
 class Host(SQLModel, table = True):
     __tablename__ = "host"
-    id: Optional[int] = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True, foreign_key = "web_user.id", on_delete = "CASCADE")
     
+class Attendee(SQLModel, table = True):
+    __tablename__ = "attendee"
+    id: Optional[int] = Field(default = None, primary_key = True, foreign_key = "web_user.id", on_delete = "CASCADE")
+
+class Calendar(SQLModel, table = True):
+    __tablename__ = "calendar"
+    id: Optional[int] = Field(default = None, primary_key  = True)
+    name: str = Field(max_length = 50, nullable = False)
+    period: datetime = Field(default = None)
+
+class Create_Schedule(SQLModel, table = True):
+    __tablename__ = "create_schedule"
+    hostID: int = Field(primary_key = True, foreign_key = "host.id", on_delete = "CASCADE")
+    calendarID: int = Field(primary_key = True, foreign_key = "calendar.id", on_delete = "CASCADE")
+    time_create: datetime = Field(default_factory = datetime.now)
+
+class Vote(SQLModel, table = True):
+    __tablename__ = "vote"
+    attendeeID: int = Field(primary_key = True, foreign_key = "attendee.id", on_delete = "CASCADE")
+    calendarID: int = Field(primary_key = True, foreign_key = "calendar.id", on_delete = "CASCADE")
